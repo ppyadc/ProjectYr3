66a6462728bc5ae70c27f6221efc1fef42cb1bb1
Elina <tubiselina@gmail.com>
2020-11-05 15:41:14 +0000

    added coordinates into consideration

--- sort_grid.m
+++ sort_grid.m
@@ -1,12 +1,17 @@
 
+function [grid, nx, ny, new_cell_lengthx, new_cell_lengthy] = sort_grid(x, y,...
+    grid_lengthx, grid_lengthy, cell_lengthx,cell_lengthy, coord_x, coord_y)
 
-function [grid, nx, ny] = sort_grid(x, y, grid_lengthx, grid_lengthy, cell_lengthx, cell_lengthy)
 %input values and sort into cell where  it's indices describe the
 %location of the x-y coordinate on the grid
 
 %cell_lengthx and cell_lengthy are approximate input values
 
 %outputs are cell of galaxy locations and dimensions of the grid
+
+%moving grid to position (0,0)
+y = y - coord_y;
+x = x - coord_x;
 
 %finding the dimensions of the cell
 nx = round(grid_lengthx./cell_lengthx); 
